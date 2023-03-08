70fd3b71902eb4d5d0699d1bbc8c630ede02ed92
eaperkowski <evan.a.perkowski@ttu.edu>
2023-03-08 14:49:26 -0600

    comment out code that writes png files

--- R_files/LxN_ncost_plots.R
+++ R_files/LxN_ncost_plots.R
@@ -33,17 +33,21 @@ df <- df %>%
 df$n.cost[c(316, 332)] <- NA
 ncost.soy <- lmer(sqrt(n.cost) ~ shade.cover * n.ppm + (1 | block),
                   data = subset(df, spp == "Soybean"))
-test(emtrends(ncost.soy, ~1, "n.ppm"))
+test(emtrends(ncost.soy, ~shade.cover, "n.ppm", 
+              at = list(shade.cover = c(0, 30, 50, 80))))
 
 df$n.cost[c(44, 271, 275, 331)] <- NA
 ncost.cotton <- lmer(log(n.cost) ~ shade.cover * n.ppm + (1 | block),
                      data = subset(df, spp == "Cotton"))
+test(emtrends(ncost.cotton, ~shade.cover, "n.ppm", 
+              at = list(shade.cover = c(0, 30, 50, 80))))
 
 ## Emmean fxns for regression lines + error ribbons
 ncost.soy.regline <- data.frame(emmeans(ncost.soy, ~shade.cover, "n.ppm",
                                     at = list(n.ppm = seq(0, 630, 5),
                                               shade.cover = c(0, 30, 50, 80)),
-                                    type = "response"))
+                                    type = "response")) %>%
+  mutate(linetype = ifelse(shade.cover == 0, "dashed", "solid"))
 ncost.cotton.regline <- data.frame(emmeans(ncost.cotton, ~shade.cover, "n.ppm",
                                         at = list(n.ppm = seq(0, 630, 5),
                                                   shade.cover = c(0, 30, 50, 80)),
@@ -81,7 +85,8 @@ ncost.nppm.soy <- ggplot(data = subset(df, spp == "Soybean"),
   geom_jitter(aes(fill = factor(shade.cover)), 
               size = 3, alpha = 0.75, width = 23.625, shape = 21) +
   geom_smooth(data = ncost.soy.regline,
-              aes(y = response, color = factor(shade.cover)), 
+              aes(y = response, color = factor(shade.cover),
+                  linetype = linetype), 
               size = 1.5, se = FALSE) +
   #geom_ribbon(data = ncost.soy.regline,
   #            aes(y = response, ymin = lower.CL, ymax = upper.CL, 
@@ -91,10 +96,12 @@ ncost.nppm.soy <- ggplot(data = subset(df, spp == "Soybean"),
                      labels = c("0", "30", "50", "80")) +
   scale_fill_manual(values = cbbPalette,
                      labels = c("0", "30", "50", "80")) +
+  scale_linetype_manual(values = c("dashed", "solid")) +
   scale_y_continuous(limits = c(0, 8), breaks = seq(0, 8, 2)) +
   labs(x = "Soil N fertilization (ppm)",
        y = NULL,
        fill = "Shade cover (%)", color = "Shade cover (%)") +
+  guides(linetype = "none") +
   theme_bw(base_size = 18) +
   theme(axis.title = element_text(face = "bold"),
         strip.background = element_blank(),
@@ -103,15 +110,25 @@ ncost.nppm.soy <- ggplot(data = subset(df, spp == "Soybean"),
   facet_grid(.~spp, labeller = labeller(spp = species.label))
 ncost.nppm.soy
 
+# png("../../compile_dissertation/ch2_LxN_Greenhouse/figs/fig1_ncost.png",
+#     height = 5, width = 12, units = "in", res = 600)
+# ggarrange(ncost.nppm.cotton, ncost.nppm.soy, common.legend = TRUE, legend = "right",
+#           align = "hv", font.label = list(size = 18))
+# dev.off()
+
 ###########################################################
 ## Whole-plant nitrogen mass
 ###########################################################
 n.acq.soy <- lmer(sqrt(n.acq) ~ shade.cover * n.ppm + (1 | block), 
                   data = subset(df, spp == "Soybean"))
+test(emtrends(n.acq.soy, ~shade.cover, "n.ppm", 
+              at = list(shade.cover = c(0, 30, 50, 80))))
 
 df$n.acq[c(286)] <- NA
 n.acq.cotton <- lmer(log(n.acq) ~ shade.cover * n.ppm + (1 | block), 
                      data = subset(df, spp == "Cotton"))
+test(emtrends(n.acq.cotton, ~shade.cover, "n.ppm", 
+              at = list(shade.cover = c(0, 30, 50, 80))))
 
 ## Emmean fxns for regression lines + error ribbons
 nacq.soy.regline <- data.frame(emmeans(n.acq.soy, ~shade.cover, "n.ppm",
@@ -178,26 +195,39 @@ nacq.nppm.soy <- ggplot(data = subset(df, spp == "Soybean"),
   facet_grid(.~spp, labeller = labeller(spp = species.label))
 nacq.nppm.soy
 
+# png("../../compile_dissertation/ch2_LxN_Greenhouse/figs/fig2_nacq.png",
+#     height = 5, width = 12, units = "in", res = 600)
+# ggarrange(nacq.nppm.cotton, nacq.nppm.soy, common.legend = TRUE, legend = "right",
+#           align = "hv", font.label = list(size = 18))
+# dev.off()
+
 ###########################################################
 ## Root carbon mass
 ###########################################################
 df$root.carbon.mass[c(86)] <- NA
 cbg.soy <- lmer(sqrt(root.carbon.mass) ~ shade.cover * n.ppm + (1 | block), 
                         data = subset(df, spp == "Soybean"))
+test(emtrends(cbg.soy, ~shade.cover, "n.ppm", 
+              at = list(shade.cover = c(0, 30, 50, 80))))
 
 df$root.carbon.mass[44] <- NA
 cbg.cotton <- lmer(sqrt(root.carbon.mass) ~ shade.cover * n.ppm + (1 | block), 
                            data = subset(df, spp == "Cotton"))
+test(emtrends(cbg.cotton, ~shade.cover, "n.ppm", 
+              at = list(shade.cover = c(0, 30, 50, 80))))
 
 ## Emmean fxns for regression lines + error ribbons
 cbg.soy.regline <- data.frame(emmeans(cbg.soy, ~shade.cover, "n.ppm",
                                         at = list(n.ppm = seq(0, 630, 5),
                                                   shade.cover = c(0, 30, 50, 80)),
-                                        type = "response"))
+                                        type = "response")) %>%
+  mutate(linetype = ifelse(shade.cover == 80, "dashed", "solid"))
+
 cbg.cotton.regline <- data.frame(emmeans(cbg.cotton, ~shade.cover, "n.ppm",
                                            at = list(n.ppm = seq(0, 630, 5),
                                                      shade.cover = c(0, 30, 50, 80)),
-                                           type = "response"))
+                                           type = "response")) %>%
+  mutate(linetype = ifelse(shade.cover == 50 | shade.cover == 80, "dashed", "solid"))
 
 
 cbg.nppm.cotton <- ggplot(data = subset(df, spp == "Cotton"), 
@@ -205,7 +235,7 @@ cbg.nppm.cotton <- ggplot(data = subset(df, spp == "Cotton"),
   geom_jitter(aes(fill = factor(shade.cover)), 
               size = 3, alpha = 0.75, width = 23.625, shape = 21) +
   geom_smooth(data = cbg.cotton.regline,
-              aes(y = response, color = factor(shade.cover)), 
+              aes(y = response, color = factor(shade.cover), linetype = linetype), 
               size = 1.5, se = FALSE) +
   #geom_ribbon(data = ncost.soy.regline,
   #            aes(y = response, ymin = lower.CL, ymax = upper.CL, 
@@ -215,10 +245,12 @@ cbg.nppm.cotton <- ggplot(data = subset(df, spp == "Cotton"),
                      labels = c("0", "30", "50", "80")) +
   scale_fill_manual(values = cbbPalette,
                     labels = c("0", "30", "50", "80")) +
+  scale_linetype_manual(values = c("dashed", "solid")) +
   scale_y_continuous(limits = c(0, 0.6), breaks = seq(0, 0.6, 0.2)) +
   labs(x = expression(bold("Soil N fertilization (ppm)")),
        y = expression(bold("Root carbon biomass (g C)")),
        color = "Shade cover (%)", fill = "Shade cover (%)") +
+  guides(linetype = "none") +
   theme_bw(base_size = 18) +
   theme(axis.title = element_text(face = "bold"),
         strip.background = element_blank(),
@@ -232,7 +264,7 @@ cbg.nppm.soy <- ggplot(data = subset(df, spp == "Soybean"),
   geom_jitter(aes(fill = factor(shade.cover)), 
               size = 3, alpha = 0.75, width = 23.625, shape = 21) +
   geom_smooth(data = cbg.cotton.regline,
-              aes(y = response, color = factor(shade.cover)), 
+              aes(y = response, color = factor(shade.cover), linetype = linetype), 
               size = 1.5, se = FALSE) +
   #geom_ribbon(data = ncost.soy.regline,
   #            aes(y = response, ymin = lower.CL, ymax = upper.CL, 
@@ -242,10 +274,12 @@ cbg.nppm.soy <- ggplot(data = subset(df, spp == "Soybean"),
                      labels = c("0", "30", "50", "80")) +
   scale_fill_manual(values = cbbPalette,
                     labels = c("0", "30", "50", "80")) +
+  scale_linetype_manual(values = c("dashed", "solid")) +
   scale_y_continuous(limits = c(0, 0.6), breaks = seq(0, 0.6, 0.2)) +
   labs(x = "Soil N fertilization (ppm)",
        y = NULL,
        color = "Shade cover (%)", fill = "Shade cover (%)") +
+  guides(linetype = "none") +
   theme_bw(base_size = 18) +
   theme(axis.title = element_text(face = "bold"),
         strip.background = element_blank(),
@@ -254,6 +288,12 @@ cbg.nppm.soy <- ggplot(data = subset(df, spp == "Soybean"),
   facet_grid(.~spp, labeller = labeller(spp = species.label))
 cbg.nppm.soy
 
+# png("../../compile_dissertation/ch2_LxN_Greenhouse/figs/fig3_rootCarbon.png",
+#     height = 5, width = 12, units = "in", res = 600)
+# ggarrange(cbg.nppm.cotton, cbg.nppm.soy, common.legend = TRUE, legend = "right",
+#           align = "hv", font.label = list(size = 18))
+# dev.off()
+
 ###########################################################
 ## Root nodule weight
 ###########################################################
@@ -261,6 +301,8 @@ df$nod.wt[c(10, 203)] <- NA
 
 nod.wgt <- lmer(sqrt(nod.wt) ~ shade.cover * n.ppm + (1 | block), 
                 data = subset(df, spp == "Soybean" & nod.wt > 0))
+test(emtrends(nod.wgt, ~shade.cover, "n.ppm", 
+              at = list(shade.cover = c(0, 30, 50, 80))))
 
 ## Emmean fxns for regression lines + error ribbons
 nod.wgt.regline <- data.frame(emmeans(nod.wgt, ~shade.cover, "n.ppm",
@@ -293,6 +335,8 @@ nod.weight.ppm
 ###########################################################
 nod.root <- lmer(sqrt(nod.root.ratio) ~ shade.cover * n.ppm + (1 | block),
                  data = subset(df, spp == "Soybean"))
+test(emtrends(nod.root, ~shade.cover, "n.ppm", 
+              at = list(shade.cover = c(0, 30, 50, 80))))
 
 ## Emmean fxns for regression lines + error ribbons
 nod.root.regline <- data.frame(emmeans(nod.root, ~shade.cover, "n.ppm",
@@ -320,3 +364,9 @@ nod.root.ppm <- ggplot(data = subset(df, spp == "Soybean" & nod.root.ratio > 0),
         strip.text = element_text(size = 20, face = "italic"),
         panel.border = element_rect(size = 1.25)) 
 nod.root.ppm
+
+# png("../../compile_dissertation/ch2_LxN_Greenhouse/figs/fig4_nodwgt.png",
+#     height = 4.5, width = 12, units = "in", res = 600)
+# ggarrange(nod.weight.ppm, nod.root.ppm, common.legend = TRUE, legend = "right",
+#           align = "hv", labels = "AUTO", font.label = list(size = 18))
+# dev.off()
